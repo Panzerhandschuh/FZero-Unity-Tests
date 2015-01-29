65e538e61a063e216dac39e2f8162213c56f4a61
Panzerhandschuh <TylerBeff@gmail.com>
2015-01-29 01:34:51 -0500

    Added more features for loading splines

--- Scripts/CollisionParser.cs
+++ Scripts/CollisionParser.cs
@@ -5,16 +5,18 @@ using System.IO;
 public class CollisionParser : MonoBehaviour
 {
 	public GameObject debugText;
+	public bool readObjects;
 
 	const int TABLE_ENTRY_SIZE = 16;
 	const int SPLINE_ENTRY_SIZE = 12;
+	const int OBJECT_ENTRY_SIZE = 64;
 	const int TRIANGLE_ENTRY_SIZE = 88;
 	const int QUAD_ENTRY_SIZE = 112;
 
 	void Start()
 	{
 		string testDirectory = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments) + "\\FZeroTests";
-		string filePath = testDirectory + "\\COLI_COURSE03,lz";
+		string filePath = testDirectory + "\\COLI_COURSE04,lz";
 		using (BinaryReader reader = new BinaryReader(File.Open(filePath, FileMode.Open)))
 		{
 			// Read spline data
@@ -24,12 +26,12 @@ public class CollisionParser : MonoBehaviour
 			ReadSplineTable(reader, splineTableSize, splineTableOffset);
 
 			// Read main mesh data
-			reader.BaseStream.Seek(100, SeekOrigin.Begin); // Seek to header info about mesh offset table
-			int tableSize = ReadInt32(reader);
-			int tableOffset = ReadInt32(reader);
-			ReadMeshTable(reader, tableSize, tableOffset);
+			//reader.BaseStream.Seek(100, SeekOrigin.Begin); // Seek to header info about mesh offset table
+			//int tableSize = ReadInt32(reader);
+			//int tableOffset = ReadInt32(reader);
+			//ReadMeshTable(reader, tableSize, tableOffset);
 
-			//// Read extra mesh data (boost pads, heal regions, jump pads, misc collisions)
+			// Read extra mesh data (boost pads, heal regions, jump pads, misc collisions)
 			reader.BaseStream.Seek(28, SeekOrigin.Begin); // Seek to header info about extra mesh offset table
 			int extraTableOffset = ReadInt32(reader);
 			ReadMeshExtraTable(reader, extraTableOffset);
@@ -38,12 +40,22 @@ public class CollisionParser : MonoBehaviour
 			//reader.BaseStream.Seek(16, SeekOrigin.Begin); // Seek to header info about unknown data table
 			//int unknownTableSize = ReadInt32(reader);
 			//int unknownTableOffset = ReadInt32(reader);
-			//ReadUnknownTablex10(reader, unknownTableSize, unknownTableOffset);
+			//ReadUnknownTable0x10(reader, unknownTableSize, unknownTableOffset);
+
+			// Read object data (boost pad locations, mines)
+			if (readObjects)
+			{
+				reader.BaseStream.Seek(72, SeekOrigin.Begin); // Seek to header info about number of objects
+				int objectTableSize = ReadInt32(reader);
+				reader.BaseStream.Seek(84, SeekOrigin.Begin); // Seek to header info about object table offset
+				int objectTableOffset = ReadInt32(reader);
+				ReadObjectTable(reader, objectTableSize, objectTableOffset);
+			}
 
 			// Read unknown data (0x84)
 			//reader.BaseStream.Seek(132, SeekOrigin.Begin);
 			//int unknownTableOffset = ReadInt32(reader);
-			//ReadUnknownTablex84(reader, unknownTableOffset);
+			//ReadUnknownTable0x84(reader, unknownTableOffset);
 		}
 	}
 
@@ -53,35 +65,36 @@ public class CollisionParser : MonoBehaviour
 		print("Spline Table Offset: " + offset);
 
 		Vector3 lastPos = Vector3.zero;
-		//Vector3 firstPos = Vector3.zero;
+		Vector3 firstPos = Vector3.zero;
 		for (int i = 0; i < numEntries; i++)
 		{
-			reader.BaseStream.Seek(offset + (SPLINE_ENTRY_SIZE * i), SeekOrigin.Begin); // Go to the current spline entry
+			reader.BaseStream.Seek(offset + (SPLINE_ENTRY_SIZE * i), SeekOrigin.Begin); // Go to the spline list
 			reader.BaseStream.Seek(4, SeekOrigin.Current); // Skip unknown data
 
-			int unknownOffset = ReadInt32(reader);
-			reader.BaseStream.Seek(unknownOffset, SeekOrigin.Begin); // Go to spline info
-			reader.BaseStream.Seek(12, SeekOrigin.Current); // Skip unknown bytes
+			int splineOffset = ReadInt32(reader);
+			Spline spline = Spline.LoadSpline(reader, splineOffset);
 
-			// Read spline tangent
-			Vector3 t = ReadVector(reader);
+			//DrawText(splineOffset.ToString("X"), v);
 
-			// Read spline position
-			Vector3 v = ReadVector(reader);
-			CreateVertex(v);
+			//reader.BaseStream.Seek(24, SeekOrigin.Current);
+			//Vector3 v2 = ReadVector(reader);
+			//v2 = new Vector3(v2.x / 10f, v2.y / 10f, v2.z / 10f);
+			//CreateVertex(v2);
 
 			// Draw spline tangent
-			Debug.DrawRay(v, t * 5f, Color.blue, 999f);
+			//Debug.DrawRay(spline.start, spline.startTangent * 5f, Color.white, 999f);
+
+			//Debug.DrawLine(v1, v2, Color.green, 999f);
 
 			// Draw spline connections
-			if (i != 0)
-				Debug.DrawLine(lastPos, v, Color.green, 999f);
+			//if (i != 0)
+			//	Debug.DrawLine(lastPos, spline.start, Color.green, 999f);
 
 			//if (i == 0)
-			//	firstPos = v;
+			//	firstPos = spline.start;
 			//else if (i == numEntries - 1) // Draw first to last connection
-			//	Debug.DrawLine(v, firstPos, Color.green, 999f);
-			lastPos = v;
+			//	Debug.DrawLine(spline.start, firstPos, Color.green, 999f);
+			//lastPos = spline.start;
 		}
 	}
 
@@ -91,10 +104,11 @@ public class CollisionParser : MonoBehaviour
 		print("Number of Mesh Table Entries: " + numEntries);
 		print("Mesh Table Offset: " + offset);
 
+		reader.BaseStream.Seek(offset, SeekOrigin.Begin); // Go to the mesh collisions table
+
 		int objectCount = 0;
 		for (int i = 0; i < numEntries; i++) // Each table entry is 16 bytes
 		{
-			reader.BaseStream.Seek(offset + (TABLE_ENTRY_SIZE * i), SeekOrigin.Begin); // Go to the current table entry
 			reader.BaseStream.Seek(12, SeekOrigin.Current); // Skip first 12 bytes of data on table entry (unknown data)
 
 			int collisionInfoOffset = ReadInt32(reader);
@@ -108,7 +122,29 @@ public class CollisionParser : MonoBehaviour
 		print("Number of mesh objects found: " + objectCount);
 	}
 
-	// Read the other table that contains offsets to triangle and quad meshes
+	void ReadCollisionInfo(BinaryReader reader, int offset)
+	{
+		print("Collision Info Offset: " + offset);
+
+		// Store stream position
+		long startOffset = reader.BaseStream.Position;
+
+		reader.BaseStream.Seek(offset, SeekOrigin.Begin); // Go to the collision info entry
+		// Collision info entries are 36 bytes long
+		reader.BaseStream.Seek(20, SeekOrigin.Current); // Skip the first 20 bytes (unknown data)
+
+		int numTriangles = ReadInt32(reader);
+		int numQuads = ReadInt32(reader);
+		int trianglesOffset = ReadInt32(reader);
+		int quadOffset = ReadInt32(reader);
+		ReadTriangles(reader, numTriangles, trianglesOffset, Color.red);
+		ReadQuads(reader, numQuads, quadOffset, Color.red);
+
+		// Restore stream position
+		reader.BaseStream.Seek(startOffset, SeekOrigin.Begin);
+	}
+
+	// Read the mesh table that contains offsets to triangle and quad meshes (boost pads, heal/dirt regions, misc collisions)
 	void ReadMeshExtraTable(BinaryReader reader, int offset)
 	{
 		print("Extra Mesh Table Offset: " + offset);
@@ -119,7 +155,7 @@ public class CollisionParser : MonoBehaviour
 		int startTriangleOffset = ReadInt32(reader);
 		int nextTriangleOffset = ReadInt32(reader);
 		int numTriangles = (nextTriangleOffset - startTriangleOffset - 24) / TRIANGLE_ENTRY_SIZE; // Find the difference between the start triangle mesh offset and the next triangle mesh offset, subtract 24 bytes (unknown data), then divide by size of triangle entry
-		ReadTriangles(reader, numTriangles, startTriangleOffset);
+		ReadTriangles(reader, numTriangles, startTriangleOffset, new Color(1f, 0.5f, 0f));
 
 		// Read quad meshes
 		reader.BaseStream.Seek(offset, SeekOrigin.Begin); // Go to start of extra mesh table
@@ -128,10 +164,10 @@ public class CollisionParser : MonoBehaviour
 		int startQuadOffset = ReadInt32(reader);
 		int nextQuadOffset = ReadInt32(reader);
 		int numQuads = (nextQuadOffset - startQuadOffset - 24) / QUAD_ENTRY_SIZE;
-		ReadQuads(reader, numQuads, startQuadOffset);
+		ReadQuads(reader, numQuads, startQuadOffset, new Color(1f, 0.25f, 0f));
 	}
 
-	void ReadUnknownTablex10(BinaryReader reader, int numEntries, int offset)
+	void ReadUnknownTable0x10(BinaryReader reader, int numEntries, int offset)
 	{
 		print("Number of Unknown Table Entries: " + numEntries);
 		print("Unknown Table Offset: " + offset);
@@ -145,7 +181,105 @@ public class CollisionParser : MonoBehaviour
 		}
 	}
 
-	void ReadUnknownTablex84(BinaryReader reader, int offset)
+	// Table for object positions (start line, mines, boosts)
+	void ReadObjectTable(BinaryReader reader, int numEntries, int offset)
+	{
+		print("Number of Object Table Entries: " + numEntries);
+		print("Object Table Offset: " + offset);
+
+		reader.BaseStream.Seek(offset, SeekOrigin.Begin); // Go to start of object table
+
+		for (int i = 0; i < numEntries; i++)
+		{
+			reader.BaseStream.Seek(12, SeekOrigin.Current); // Skip 12 unknown bytes
+
+			Vector3 pos = ReadVector(reader);
+			CreateVertex(pos, new Vector3(5f, 5f, 5f), Color.red);
+
+			reader.BaseStream.Seek(24, SeekOrigin.Current); // Skip 24 unknown bytes
+			int extraInfoOffset = ReadInt32(reader);
+			if (extraInfoOffset != 0)
+				ReadObjectExtraInfo(reader, extraInfoOffset);
+
+			reader.BaseStream.Seek(8, SeekOrigin.Current); // Skip 8 unknown bytes
+			int orientationOffset = ReadInt32(reader);
+			ReadObjectOrientation(reader, pos, orientationOffset);
+		}
+	}
+
+	// Unknown info about the object
+	void ReadObjectExtraInfo(BinaryReader reader, int offset)
+	{
+		//print("Object Extra Info Offset: " + offset);
+
+		// Store stream position
+		long startOffset = reader.BaseStream.Position;
+
+		reader.BaseStream.Seek(offset, SeekOrigin.Begin);
+		reader.BaseStream.Seek(28, SeekOrigin.Current); // Skip 28 unknown/null bytes
+
+		const int numAddresses = 33; // Number of 8 byte length + address pairs to check
+		for (int i = 0; i < numAddresses; i++)
+		{
+			int extraSize = ReadInt32(reader);
+			int extraOffset = ReadInt32(reader);
+			if (extraOffset != 0)
+				ReadObjectExtraItems(reader, extraSize, extraOffset);
+		}
+
+		// Restore stream position
+		reader.BaseStream.Seek(startOffset, SeekOrigin.Begin);
+	}
+
+	// A list of unknown items related to the object info
+	void ReadObjectExtraItems(BinaryReader reader, int numEntries, int offset)
+	{
+		//print(numEntries + " " + offset);
+
+		// Store stream position
+		long startOffset = reader.BaseStream.Position;
+
+		reader.BaseStream.Seek(offset, SeekOrigin.Begin);
+		for (int i = 0; i < numEntries; i++)
+		{
+			reader.BaseStream.Seek(8, SeekOrigin.Current); // Skip 8 unknown bytes
+			
+			Vector3 v = ReadVector(reader);
+			//CreateVertex(v, new Vector3(5f, 5f, 5f), Color.green);
+		}
+
+		// Restore stream position
+		reader.BaseStream.Seek(startOffset, SeekOrigin.Begin);
+	}
+
+	// The rotation of the objects?
+	void ReadObjectOrientation(BinaryReader reader, Vector3 position, int offset)
+	{
+		// Store stream position
+		long startOffset = reader.BaseStream.Position;
+
+		reader.BaseStream.Seek(offset, SeekOrigin.Begin); // Go to object orientation data
+
+		Vector3 v1 = ReadVector(reader).normalized;
+		Debug.DrawRay(position, v1 * 5f, Color.yellow, 999f);
+
+		reader.BaseStream.Seek(4, SeekOrigin.Current); // Skip 4 unknown bytes
+
+		Vector3 v2 = ReadVector(reader).normalized;
+		Debug.DrawRay(position, v2 * 5f, Color.yellow, 999f);
+
+		reader.BaseStream.Seek(4, SeekOrigin.Current); // Skip 4 unknown bytes
+
+		Vector3 v3 = ReadVector(reader).normalized;
+		Debug.DrawRay(position, v3 * 5f, Color.yellow, 999f);
+
+		//reader.BaseStream.Seek(4, SeekOrigin.Current); // Skip 4 unknown bytes
+
+		// Restore stream position
+		reader.BaseStream.Seek(startOffset, SeekOrigin.Begin);
+	}
+
+	void ReadUnknownTable0x84(BinaryReader reader, int offset)
 	{
 		print("Unknown Table Offset: " + offset);
 
@@ -157,23 +291,7 @@ public class CollisionParser : MonoBehaviour
 		}
 	}
 
-	void ReadCollisionInfo(BinaryReader reader, int offset)
-	{
-		print("Collision Info Offset: " + offset);
-
-		reader.BaseStream.Seek(offset, SeekOrigin.Begin); // Go to the collision info entry
-		// Collision info entries are 36 bytes long
-		reader.BaseStream.Seek(20, SeekOrigin.Current); // Skip the first 20 bytes (unknown data)
-
-		int numTriangles = ReadInt32(reader);
-		int numQuads = ReadInt32(reader);
-		int trianglesOffset = ReadInt32(reader);
-		int quadOffset = ReadInt32(reader);
-		ReadTriangles(reader, numTriangles, trianglesOffset);
-		ReadQuads(reader, numQuads, quadOffset);
-	}
-
-	void ReadTriangles(BinaryReader reader, int numTriangles, int offset)
+	void ReadTriangles(BinaryReader reader, int numTriangles, int offset, Color color)
 	{
 		print("Number of Triangles: " + numTriangles);
 		print("Triangles Offset: " + offset);
@@ -192,12 +310,12 @@ public class CollisionParser : MonoBehaviour
 
 			Vector3 v2 = ReadVector(reader);
 			CreateVertex(v2);
-			Debug.DrawLine(v1, v2, Color.red, 999f);
+			Debug.DrawLine(v1, v2, color, 999f);
 
 			Vector3 v3 = ReadVector(reader);
 			CreateVertex(v3);
-			Debug.DrawLine(v2, v3, Color.red, 999f);
-			Debug.DrawLine(v3, v1, Color.red, 999f);
+			Debug.DrawLine(v2, v3, color, 999f);
+			Debug.DrawLine(v3, v1, color, 999f);
 
 			// Display normal
 			Vector3 nPos = (v1 + v2 + v3) / 3f;
@@ -223,7 +341,7 @@ public class CollisionParser : MonoBehaviour
 		Debug.DrawLine(v3, v1, Color.red, 999f);
 	}
 
-	void ReadQuads(BinaryReader reader, int numQuads, int offset)
+	void ReadQuads(BinaryReader reader, int numQuads, int offset, Color color)
 	{
 		print("Number of Quads: " + numQuads);
 		print("Quads Offset: " + offset);
@@ -242,16 +360,16 @@ public class CollisionParser : MonoBehaviour
 
 			Vector3 v2 = ReadVector(reader);
 			CreateVertex(v2);
-			Debug.DrawLine(v1, v2, Color.red, 999f);
+			Debug.DrawLine(v1, v2, color, 999f);
 
 			Vector3 v3 = ReadVector(reader);
 			CreateVertex(v3);
-			Debug.DrawLine(v2, v3, Color.red, 999f);
+			Debug.DrawLine(v2, v3, color, 999f);
 
 			Vector3 v4 = ReadVector(reader);
 			CreateVertex(v4);
-			Debug.DrawLine(v3, v4, Color.red, 999f);
-			Debug.DrawLine(v4, v1, Color.red, 999f);
+			Debug.DrawLine(v3, v4, color, 999f);
+			Debug.DrawLine(v4, v1, color, 999f);
 
 			// Display normal
 			Vector3 nPos = (v1 + v2 + v3 + v4) / 4f;
@@ -282,11 +400,22 @@ public class CollisionParser : MonoBehaviour
 	}
 
 	// Used for visualizing vertices
-	void CreateVertex(Vector3 position)
+	GameObject CreateVertex(Vector3 position)
 	{
 		GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
 		obj.transform.position = position;
 		obj.transform.localScale = new Vector3(2f, 2f, 2f);
+		return obj;
+	}
+
+	GameObject CreateVertex(Vector3 position, Vector3 scale, Color color)
+	{
+		GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		obj.transform.position = position;
+		obj.transform.localScale = scale;
+		obj.renderer.material.color = color;
+		obj.name = "Test";
+		return obj;
 	}
 
 	void DrawText(string text, Vector3 position)
@@ -296,19 +425,19 @@ public class CollisionParser : MonoBehaviour
 		obj.GetComponent<TextMesh>().text = text;
 	}
 
-	Vector3 ReadVector(BinaryReader reader)
+	public static Vector3 ReadVector(BinaryReader reader)
 	{
 		return new Vector3(-ReadSingle(reader), ReadSingle(reader), ReadSingle(reader));
 	}
 
-	int ReadInt32(BinaryReader reader)
+	public static int ReadInt32(BinaryReader reader)
 	{
 		byte[] bytes = reader.ReadBytes(4);
 		Array.Reverse(bytes); // Swap endianness
 		return BitConverter.ToInt32(bytes, 0);
 	}
 
-	float ReadSingle(BinaryReader reader)
+	public static float ReadSingle(BinaryReader reader)
 	{
 		byte[] bytes = reader.ReadBytes(4);
 		Array.Reverse(bytes); // Swap endianness
---
+++ Scripts/Spline.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.IO;
+
+public class Spline : MonoBehaviour
+{
+	public string address;
+	public int unknown1;
+	public int unknown2;
+	public int unknown3;
+	public Vector3 startTangent;
+	public Vector3 start;
+	public int unknown4;
+	public Vector3 endTangent;
+	public Vector3 end;
+	public int unknown5;
+	public int unknown6;
+	public float width;
+	public int type;
+
+	public static Spline LoadSpline(BinaryReader reader, int offset)
+	{
+		GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		//GameObject end = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		Spline spline = obj.AddComponent<Spline>();
+
+		reader.BaseStream.Seek(offset, SeekOrigin.Begin); // Go to spline info
+		spline.address = reader.BaseStream.Position.ToString("X");
+		spline.unknown1 = CollisionParser.ReadInt32(reader);
+		spline.unknown2 = CollisionParser.ReadInt32(reader);
+
+		spline.unknown3 = CollisionParser.ReadInt32(reader);
+		spline.startTangent = CollisionParser.ReadVector(reader);
+		spline.start = CollisionParser.ReadVector(reader);
+
+		spline.unknown4 = CollisionParser.ReadInt32(reader);
+		spline.endTangent = CollisionParser.ReadVector(reader);
+		spline.end = CollisionParser.ReadVector(reader);
+
+		spline.unknown5 = CollisionParser.ReadInt32(reader);
+		spline.unknown6 = CollisionParser.ReadInt32(reader);
+		spline.width = CollisionParser.ReadSingle(reader);
+		spline.type = CollisionParser.ReadInt32(reader);
+
+		obj.transform.position = spline.start;
+		//end.transform.position = spline.start1;
+		Debug.DrawLine(spline.start, spline.end, Color.green, 999f);
+		Debug.DrawRay(spline.start, spline.startTangent, Color.white, 999f);
+		Debug.DrawRay(spline.end, spline.endTangent, Color.white, 999f);
+
+		return spline;
+	}
+}
