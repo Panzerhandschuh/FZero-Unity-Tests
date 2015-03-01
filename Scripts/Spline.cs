6fe87aba2f0abbec83737221753a893d7fbb67c7
Raphael Tetreault <raphaeltetreault@hotmail.com>
2015-03-01 15:21:22 -0500

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 852a63ed4291944c692003b761611b7017f8d8f2, reversing
    changes made to 8f2f13714df956efce6e47ade575eb5953c0cbd9.

--- Scripts/Spline.cs
+++ Scripts/Spline.cs
@@ -4,21 +4,18 @@ using System.IO;
 public class Spline : MonoBehaviour
 {
 	public int address;
-	public float trackOffset1; // Moves the track collisions forward/back?
-	public float trackOffset2; // Moves the track collisions forward/back?
-	public float unknown3;
-	public Vector3 startTangent; // Rotating the tangent affects the collisions
-	public Vector3 start; // Changing this does not change collisions, but it affects what parts of the track kill you (maybe related to activating checkpoints)
-	public float unknown4;
-	public Vector3 endTangent; // Rotating the tangent affects the collisions
-	public Vector3 end; // Changing this does not change collisions, but it affects what parts of the track kill you (maybe related to activating checkpoints)
-	public float unknown5; // Messes with CPU AI, but does not affect collisions
-	public float unknown6; // Messes with CPU AI, but does not affect collisions
-	public float width; // Seems to be related to track width, but changing this doesn't seem to do anything
-	public byte startConnected; // Flag denoting whether or not the current spline has a connection with the previous spline (0 when there is no previous connection such as right after a dive)
-	public byte endConnected; // Flag denoting whether or not the current spline has a connection with the next spline (0 when there is no next connection such as right before a dive)
-	public byte flag3; // Always seems to be 0
-	public byte flag4; // Always seems to be 0
+	public int unknown1;
+	public int unknown2;
+	public int unknown3;
+	public Vector3 startTangent;
+	public Vector3 start;
+	public int unknown4;
+	public Vector3 endTangent;
+	public Vector3 end;
+	public int unknown5;
+	public int unknown6;
+	public float width;
+	public int type;
 
 	public static Spline LoadSpline(BinaryReader reader, int offset)
 	{
@@ -29,32 +26,27 @@ public class Spline : MonoBehaviour
 
 		reader.BaseStream.Seek(offset, SeekOrigin.Begin); // Go to spline info
 		spline.address = (int)reader.BaseStream.Position;
-		spline.trackOffset1 = BinarySerializer.ReadSingle(reader);
-		spline.trackOffset2 = BinarySerializer.ReadSingle(reader);
+		spline.unknown1 = BinarySerializer.ReadInt32(reader);
+		spline.unknown2 = BinarySerializer.ReadInt32(reader);
 
-		spline.unknown3 = BinarySerializer.ReadSingle(reader);
+		spline.unknown3 = BinarySerializer.ReadInt32(reader);
 		spline.startTangent = BinarySerializer.ReadVector(reader);
 		spline.start = BinarySerializer.ReadVector(reader);
 
-		spline.unknown4 = BinarySerializer.ReadSingle(reader);
+		spline.unknown4 = BinarySerializer.ReadInt32(reader);
 		spline.endTangent = BinarySerializer.ReadVector(reader);
 		spline.end = BinarySerializer.ReadVector(reader);
 
-		spline.unknown5 = BinarySerializer.ReadSingle(reader);
-		spline.unknown6 = BinarySerializer.ReadSingle(reader);
+		spline.unknown5 = BinarySerializer.ReadInt32(reader);
+		spline.unknown6 = BinarySerializer.ReadInt32(reader);
 		spline.width = BinarySerializer.ReadSingle(reader);
-		spline.startConnected = reader.ReadByte();
-		spline.endConnected = reader.ReadByte();
-		spline.flag3 = reader.ReadByte();
-		spline.flag4 = reader.ReadByte();
+		spline.type = BinarySerializer.ReadInt32(reader);
 
-		if (spline.flag4 == 1)
-			print(spline.address);
-		obj.transform.position = (spline.start + spline.end) / 2f;
-		Debug.DrawLine(obj.transform.position, spline.start, Color.green, 999f);
-		Debug.DrawLine(obj.transform.position, spline.end, Color.green, 999f);
-		Debug.DrawRay(obj.transform.position, spline.startTangent, Color.white, 999f);
-		Debug.DrawRay(obj.transform.position, spline.endTangent, Color.white, 999f);
+		obj.transform.position = spline.start;
+		//end.transform.position = spline.start1;
+		Debug.DrawLine(spline.start, spline.end, Color.green, 999f);
+		Debug.DrawRay(spline.start, spline.startTangent, Color.white, 999f);
+		Debug.DrawRay(spline.end, spline.endTangent, Color.white, 999f);
 
 		return spline;
 	}
@@ -62,8 +54,8 @@ public class Spline : MonoBehaviour
 	public static void WriteSpline(BinaryWriter writer, Spline spline)
 	{
 		writer.BaseStream.Seek(spline.address, SeekOrigin.Begin);
-		BinarySerializer.Write(writer, spline.trackOffset1);
-		BinarySerializer.Write(writer, spline.trackOffset2);
+		BinarySerializer.Write(writer, spline.unknown1);
+		BinarySerializer.Write(writer, spline.unknown2);
 
 		BinarySerializer.Write(writer, spline.unknown3);
 		BinarySerializer.Write(writer, spline.startTangent);
@@ -76,9 +68,6 @@ public class Spline : MonoBehaviour
 		BinarySerializer.Write(writer, spline.unknown5);
 		BinarySerializer.Write(writer, spline.unknown6);
 		BinarySerializer.Write(writer, spline.width);
-		writer.Write(spline.startConnected);
-		writer.Write(spline.endConnected);
-		writer.Write(spline.flag3);
-		writer.Write(spline.flag4);
+		BinarySerializer.Write(writer, spline.type);
 	}
 }
